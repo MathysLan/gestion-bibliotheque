5066171c58e8a243bb4e474c872ffe9f3f4b3d98
Mathys Langiny <mathys.langiny@gmail.com>
2024-10-21 17:55:20 +0200

    Ajout de l'entité Abonne permettant plus tard de gérer les abonnés

---
+++ src/Entity/Abonne.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AbonneRepository;
+use DateTimeInterface;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AbonneRepository::class)]
+class Abonne
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 180)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $adresse = null;
+
+    #[ORM\Column(length: 20, nullable: true)]
+    private ?string $telephone = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?DateTimeInterface $dateInscription = null;
+
+    #[ORM\Column]
+    private ?int $nombreEmpruntAutorise = null;
+
+    #[ORM\ManyToOne(targetEntity: Livre::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private Livre $livre;
+
+    #[ORM\ManyToOne(targetEntity: Abonne::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private Abonne $abonne;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(?string $adresse): static
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?string
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(?string $telephone): static
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getDateInscription(): ?\DateTimeInterface
+    {
+        return $this->dateInscription;
+    }
+
+    public function setDateInscription(\DateTimeInterface $dateInscription): static
+    {
+        $this->dateInscription = $dateInscription;
+
+        return $this;
+    }
+
+    public function getNombreEmpruntAutorise(): ?int
+    {
+        return $this->nombreEmpruntAutorise;
+    }
+
+    public function setNombreEmpruntAutorise(int $nombreEmpruntAutorise): static
+    {
+        $this->nombreEmpruntAutorise = $nombreEmpruntAutorise;
+
+        return $this;
+    }
+}
