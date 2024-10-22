7768e8a7753b840093d4c294bfd6c0e71f51f705
Mathys Langiny <mathys.langiny@gmail.com>
2024-10-22 22:39:42 +0200

    Ajout d'une route permettant de connaitre les détails du livre

--- src/Controller/LivreController.php
+++ src/Controller/LivreController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Livre;
 use App\Repository\LivreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,5 +15,11 @@ class LivreController extends AbstractController
     {
         $livres = $livreRepository->findAll();
         return $this->render('livre/liste_livres.html.twig', ['livres' => $livres]);
+    }
+
+    #[Route('/livres/{id}', name: 'app_liste_livre')]
+    public function show(Livre $livre): Response
+    {
+        return $this->render('livre/show.html.twig', ['livre' => $livre]);
     }
 }
