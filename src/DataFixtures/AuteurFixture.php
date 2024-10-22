dd74de938691c783861f307a8f21b017da3a6ac1
Mathys Langiny <mathys.langiny@gmail.com>
2024-10-22 21:36:10 +0200

    Demande de génération de 2 auteur

--- src/DataFixtures/AuteurFixture.php
+++ src/DataFixtures/AuteurFixture.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\AbonneFactory;
+use App\Factory\AuteurFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +11,6 @@ class AuteurFixture extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+        AuteurFactory::createMany(2);
     }
 }
